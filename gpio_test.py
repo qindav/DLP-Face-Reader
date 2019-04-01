6db5e05592e0adaafdec300cfa7b0749c9ee4f1d
mdkroesche <47091762+mdkroesche@users.noreply.github.com>
2019-04-01 12:39:21 -0500

    Add files via upload

---
+++ gpio_test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# Raspberry Pi I/O test
+# Matthew Kroesche
+# ECEN 404
+
+from DLPScanner.gpio import *
+
+io = IO(None)
+io.init()
+
+while True:
+    s = input()
+    if s.startswith('P'):
+        GPIO.output(POWER_LED, GPIO.HIGH)
+    elif s.startswith('p'):
+        GPIO.output(POWER_LED, GPIO.LOW)
+    elif s.startswith('W'):
+        GPIO.output(WIFI_LED, GPIO.HIGH)
+    elif s.startswith('w'):
+        GPIO.output(WIFI_LED, GPIO.LOW)
+    elif s.startswith('U'):
+        GPIO.output(USB_LED, GPIO.HIGH)
+    elif s.startswith('u'):
+        GPIO.output(USB_LED, GPIO.LOW)
+    elif s.startswith('B'):
+        GPIO.output(BUSY_LED, GPIO.HIGH)
+    elif s.startswith('b'):
+        GPIO.output(BUSY_LED, GPIO.LOW)
+    elif s.startswith('E'):
+        GPIO.output(ERROR_LED, GPIO.HIGH)
+    elif s.startswith('e'):
+        GPIO.output(ERROR_LED, GPIO.LOW)
+    elif s.startswith('1'):
+        print('Power button state: %d' % GPIO.input(POWER_BUTTON))
+    elif s.startswith('2'):
+        print('Snapshot button state: %d' % GPIO.input(SNAPSHOT_BUTTON))
+    elif not s:
+        io.quit()
+        break
